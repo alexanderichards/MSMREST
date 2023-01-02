328f916173a5d626a613ed5804efeebd3c7fd050
Alex R <alexanderichards@hotmail.com>
2023-01-02 11:41:31 -0500

    Agregar archivos de proyecto.

---
+++ Mango.Services.ProductAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Mango.Services.ProductAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
